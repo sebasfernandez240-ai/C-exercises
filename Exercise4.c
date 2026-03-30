d84278cb12f46a89d1443378e05dd20dc3025862
Sebas <sebasfernandez240@gmail.com>
2026-03-30 14:40:21 -0600

    Basic arithmetic operation

---
+++ Exercise4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float op = pow(((3.0 + 2.0) / (2.0 * 5.0)), 2);
+    printf("%f\n", op);
+    return 0;
+}
---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    char helloworld[11] = "Hello World";
+    printf("%s\n", helloworld);
+    return 0;
+}
